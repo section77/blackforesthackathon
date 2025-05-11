dcbeacedd93f57b80c46f891b47fedfa9a9cc6e1
Justin Otherguy <justin@justinotherguy.org>
2025-05-11 12:59:17 +0200

    remove token (that no longer exists anyway)

--- code/hassio-api.py
+++ code/hassio-api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://192.168.1.102:8123/api/states"
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2OTRlMjcyZmZmYzM0YmMzOWE0ZWU3NmU4NmViNzUxNSIsImlhdCI6MTc0NjgyMTg5NywiZXhwIjoyMDYyMTgxODk3fQ.r2Jij93SlvTU61LNilRYC-OuW5gE0mUjgqes5HWmHRI",
+    "Authorization": "Bearer <token>",
 }
 response = requests.request("GET", url, headers=headers)
 
